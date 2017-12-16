a10c5ae725e8b0f92cd45e1cccca1663cb3fe9fb
Darwin Batres <darwin.ba3@gmail.com>
2017-12-16 17:13:24 -0600

    hooked up authentication to protect routes

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 const controller = require('../controllers/ideas');
+const { ensureAuthenticated } = require('../helpers/auth');
 
 const router = express.Router();
 
-router.get('/', controller.getAll);
-router.get('/add', controller.add);
-router.get('/edit/:id', controller.edit);
-router.post('/', controller.post);
-router.put('/:id', controller.put);
-router.delete('/:id', controller.delete);
+router.get('/', ensureAuthenticated, controller.getAll);
+router.get('/add', ensureAuthenticated, controller.add);
+router.get('/edit/:id', ensureAuthenticated, controller.edit);
+router.post('/', ensureAuthenticated, controller.post);
+router.put('/:id', ensureAuthenticated, controller.put);
+router.delete('/:id', ensureAuthenticated, controller.delete);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.post('/', controller.post);
 router.get('/login', controller.login);
+router.post('/login', controller.postLogin);
+router.get('/logout', controller.logout);
 router.get('/register', controller.register);
 
 module.exports = router;
