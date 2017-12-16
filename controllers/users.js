93b6e941773526f469d72a8861900dd9c081a74d
Darwin Batres <darwin.ba3@gmail.com>
2017-12-16 14:18:19 -0600

    Add new logic to post information when registering new users

--- controllers/users.js
+++ controllers/users.js
@@ -1,56 +1,19 @@
+const User = require('../models/User');
+const { validateUser } = require('../helpers/validateUser');
 // const logger = require('../utils/logger');
 
-module.exports.login = (req, res) => {
-  res.render('users/login');
-};
+module.exports.login = (req, res) => res.render('users/login');
 
-module.exports.register = (req, res) => {
-  res.render('users/register');
-};
+module.exports.register = (req, res) => res.render('users/register');
 
-module.exports.post = (req, res) => {
-  const errors = [];
+module.exports.post = async (req, res) => {
   const {
     name,
     email,
     password,
-    passwordVerify,
   } = req.body;
 
-  if (!name) {
-    errors.push({
-      property: 'name',
-      message: 'name is a required field',
-    });
-  }
-
-  if (!email) {
-    errors.push({
-      property: 'email',
-      message: 'email is a required field',
-    });
-  }
-
-  if (!password) {
-    errors.push({
-      property: 'password',
-      message: 'password is a required field',
-    });
-  }
-
-  if (!passwordVerify) {
-    errors.push({
-      property: 'password_verify',
-      message: 'verify password is a required field',
-    });
-  }
-
-  if (passwordVerify !== password) {
-    errors.push({
-      property: 'password',
-      message: 'both passwords have to match',
-    });
-  }
+  const errors = validateUser(req.body);
 
   if (errors.length > 0) {
     res.render('users/register', {
@@ -59,6 +22,25 @@ module.exports.post = (req, res) => {
       email,
     });
   } else {
-    res.send({ message: 'success' });
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    try {
+      const userExist = await User.find({ email });
+      if (!userExist) {
+        await newUser.save();
+        req.flash('success_msg', 'Register successfully, please login now');
+        res.redirect('/users/login');
+      } else {
+        req.flash('error_msg', 'email already registered');
+        res.redirect('/users/register');
+      }
+    } catch (err) {
+      req.flash('error_msg', `There was an error while trying to register, try again ${err.message}`);
+      res.redirect('/users/register');
+    }
   }
 };
